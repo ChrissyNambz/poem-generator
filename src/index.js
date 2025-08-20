a8bb090d85b408aa8fc5e8667cc3a365eff684fa
ChrissyNambz <christine.nambatya@gmail.com>
2025-08-20 17:06:42 +0100

    Added Basic
    Added event listener & the Typewriter Plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-//alert("Hello world")
+
+function generatePoem(event){
+   event.preventDefault();
+   
+  new Typewriter("#poem-box", {
+    strings: [" The morning Rush..."],
+    autoStart: true,
+    delay:1,
+    cursor:""
+  });
+}
+
+
+let poemFormElement=document.querySelector("#poem-form");
+poemFormElement.addEventListener("submit",generatePoem);
+
