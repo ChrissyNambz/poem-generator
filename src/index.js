c96f7fff917a0fb21eba14d84a48284656f4490a
ChrissyNambz <christine.nambatya@gmail.com>
2025-08-24 18:17:51 +0100

    Integrated API to Java script
    Added an Api Key
    Called API Key
    added prompts and contexts to API Key.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-
-function generatePoem(event){
-   event.preventDefault();
-   
+function displayPoem(response) {
   new Typewriter("#poem-box", {
-    strings: [" The morning Rush..."],
+    strings: response.data.answer,
     autoStart: true,
-    delay:1,
-    cursor:""
+    delay: 1.5,
+    cursor: "",
   });
 }
+function generatePoem(event) {
+  event.preventDefault();
 
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "t6468120b34dd22a5f93b7f863a2oc0a";
+  let prompt = `user intructions: Generate a Poem about anything in ${instructionsInput.value}`;
+  let context =
+    "You are a Poems expert and love to write short poems. Your mission is to generate a four line poem in basic HTML and seperate each line with <br/>.Please make sure you follow the user instructions and do not include a title to the poem. Sign the Poem with 'she codes AI' inside <strong> element at the end. ";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key={apiKey}`;
 
-let poemFormElement=document.querySelector("#poem-form");
-poemFormElement.addEventListener("submit",generatePoem);
+  axios.get(apiURL).then(displayPoem);
+}
 
+let poemFormElement = document.querySelector("#poem-form");
+poemFormElement.addEventListener("submit", generatePoem);
